bc2431c4b9d6e0465dc33632d7763641ebd7f44d
Paul <soapmctravich@gmail.com>
2015-04-08 21:40:18 +0200

    :spades: Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var ejs = require('ejs')
+var bodyParser = require('body-parser')
+var fs   = require('fs')
+var session = require('cookie-session')
+var sha256 = require('js-sha256')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var app = express()
+
+app.use(session({ secret: 's3cr3tind3chiffrabl3' }))
+
+.use(express.static(__dirname + '/static'))
+
+.get('/', function(req, res){
+    res.render('index.ejs', { session: req.session })
+})
+
+app.listen(8080)
