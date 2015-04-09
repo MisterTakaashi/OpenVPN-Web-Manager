81e6f782bf000e5aeb11cc082075ab21a4eb44c1
Paul <soapmctravich@gmail.com>
2015-04-09 16:24:08 +0200

    :lock: Système de connexion terminé

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var ejs = require('ejs')
 var bodyParser = require('body-parser')
+var yaml = require('js-yaml');
 var fs   = require('fs')
 var session = require('cookie-session')
 var sha256 = require('js-sha256')
@@ -15,6 +16,34 @@ app.use(session({ secret: 's3cr3tind3chiffrabl3' }))
 
 .get('/', function(req, res){
     res.render('index.ejs', { session: req.session })
+})
+
+.post('/login', urlencodedParser, function(req, res){
+    /*console.log(req.body.inputEmail)
+    console.log(req.body.inputPassword)*/
+    var email = req.body.inputEmail.toLowerCase()
+    var password = req.body.inputPassword
+    var hashedPassword = sha256(req.body.inputPassword)
+    fs.readdir(__dirname + '/static/members', function(err, files){
+        for (var i = 0; i < files.length; i++) {
+            if (files[i] == email){
+                console.log("Tentative de connexion de l\'utilisateur '"+email+"'")
+                try {
+                    var doc = yaml.safeLoad(fs.readFileSync(__dirname + '/static/members/' + files[i] + '/user.yml', 'utf8'));
+                    /*console.log(doc)
+                    console.log(doc.pass + '|' + hashedPassword)*/
+                    if (doc.pass == hashedPassword){
+                        console.log("Connexion de '"+doc.email+"' réussie")
+                        req.session.email = doc.email
+                        console.log(req.session)
+                        res.redirect('/')
+                    }
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        }
+    })
 })
 
 .get('/login', function(req, res){
