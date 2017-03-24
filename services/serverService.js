e0ff0a94c1e3b7fbd1396aebe2ac6ff1060b5740
MisterTakaashi <soapmctravich@gmail.com>
2017-03-24 01:56:29 +0100

    Ajout services pour simplification actions VPN

---
+++ services/serverService.js
@@ -0,0 +1,37 @@
+var tcpp = require('tcp-ping');
+var exec = require('child_process').exec;
+
+exports.getVpnStatus = function (callback) {
+  var vpnStatus = false;
+
+  this.canPing((ping) => {
+    // On poursuit les test
+    exec("ps ax | grep openvpn", function (error, stdout, stderr) {
+      if (error !== null) {
+        callback({ping: ping, vpn: false});
+      }else{
+        if (!stdout.includes("ovpn-server")){
+          try{
+            var usersOnline = fs.readFileSync('/var/log/openvpn-status.log', 'utf8');
+            usersOnline = usersOnline.split("Connected Since")[1].split("ROUTING TABLE")[0].split('\n');
+            var nbrUsersOnline = usersOnline.length - 2;
+          }catch(e){
+            var usersOnline = null;
+            var nbrUsersOnline = 0;
+          }
+          callback({ping: ping, vpn: true, nbrUsersOnline: nbrUsersOnline});
+        }
+      }
+    });
+  });
+}
+
+exports.canPing = function (callback) {
+  tcpp.ping({address: '8.8.8.8', port: 53, attempts: 3, timeout: 2000}, function(err, data) {
+    if (err){
+      callback('NaN');
+    }else{
+      callback(Math.floor(data.avg));
+    }
+  });
+}
